4c1268b13de2110b80535af158064019f9971921
Alissonf94 <alissonfernandesxx55@gmail.com>
2024-06-25 16:47:26 -0300

    emprestarLivros

--- src/Emprestimos.java
+++ src/Emprestimos.java
@@ -1,31 +1,44 @@
-import java.util.Date;
-
 public class Emprestimos {
-    String[] livrosDisponiveis;
+    String[] livros;
     String[] nomesLeitores;
     String[] devolucao;
     boolean[] estaEmprestado;
 
     public Emprestimos(String[] livrosDisponiveis, String[] nomesLeitores, String[] devolucao) {
-        this.livrosDisponiveis = livrosDisponiveis;
+        this.livros = livrosDisponiveis;
         this.nomesLeitores = nomesLeitores;
         this.devolucao = devolucao;
     }
 
     public void listarCatalogo(){
         System.out.println("Livros disponiveis: ");
-        for(String livros : livrosDisponiveis){
+        for(String livros : livros){
             System.out.println(livros);
         }
     }
 
     public void listarLivrosEmprestados(){
         System.out.println("Livros Emprestados: ");
-        for (int i = 0; i < livrosDisponiveis.length; i++) {
+        for (int i = 0; i < livros.length; i++) {
             if (estaEmprestado[i] == true){
-                System.out.println("Nome livro: " + livrosDisponiveis[i]);
+                System.out.println("Nome livro: " + livros[i]);
                 System.out.println("Nome do leitor: " + nomesLeitores[i]);
                 System.out.println("Data de devolução: " + devolucao[i]);
+            }
+        }
+    }
+
+    public void emprestarLivro (String nomeDoLivro, String nomeDoLeitor, String dataDeDevolucao){
+        for (int x = 0; x < livros.length; x++){
+            if (estaEmprestado[x] == false){
+                if(livros[x].equalsIgnoreCase(nomeDoLivro)) {
+                    estaEmprestado[x] = true;
+                    devolucao[x] = dataDeDevolucao;
+                    System.out.println("O livro " + nomeDoLivro + " foi emprestado ao leitor " + nomeDoLeitor );
+                }
+            }
+            else{
+                System.out.println("O livro não esta disponível.");
             }
         }
     }
