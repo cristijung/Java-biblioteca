b24bd5d7b1b6f45da2eb30c0fae5d38053cdf809
Giovani Fogarolli <fogarolligiovani@gmail.com>
2024-06-25 16:28:32 -0300

    Adicao metodos de listagem

--- src/Emprestimos.java
+++ src/Emprestimos.java
@@ -1,14 +1,33 @@
 import java.util.Date;
 
 public class Emprestimos {
-    private String[] livrosDisponiveis;
-    private String[] nomesLeitores;
-    private String[] devolucao;
+    String[] livrosDisponiveis;
+    String[] nomesLeitores;
+    String[] devolucao;
+    boolean[] estaEmprestado;
 
     public Emprestimos(String[] livrosDisponiveis, String[] nomesLeitores, String[] devolucao) {
         this.livrosDisponiveis = livrosDisponiveis;
         this.nomesLeitores = nomesLeitores;
         this.devolucao = devolucao;
+    }
+
+    public void listarCatalogo(){
+        System.out.println("Livros disponiveis: ");
+        for(String livros : livrosDisponiveis){
+            System.out.println(livros);
+        }
+    }
+
+    public void listarLivrosEmprestados(){
+        System.out.println("Livros Emprestados: ");
+        for (int i = 0; i < livrosDisponiveis.length; i++) {
+            if (estaEmprestado[i] == true){
+                System.out.println("Nome livro: " + livrosDisponiveis[i]);
+                System.out.println("Nome do leitor: " + nomesLeitores[i]);
+                System.out.println("Data de devolução: " + devolucao[i]);
+            }
+        }
     }
 
 }
